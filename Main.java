0e623b2a1a35b0566eb30540df80a2ce3c0237b1
Vova <100154787+grrave@users.noreply.github.com>
2022-05-27 11:16:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package ЕКЗ123;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = new int[10][];
+        array [0] = new int [5];
+        array [1] = new int [5];
+        array [2] = new int [5];
+        array [3] = new int [5];
+        array [4] = new int [8];
+        array [5] = new int [8];
+        array [6] = new int [3];
+        array [7] = new int [3];
+        array [8] = new int [9];
+        array [9] = new int [9];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                array[i][j]=(int)(Math.random()*28);
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
